37236898241e194c80ccee8cbfb5106e1dd45be3
David Katua <149246422+Katua094@users.noreply.github.com>
2025-01-16 17:58:26 +0300

    Update bind.js

--- bind.js
+++ bind.js
@@ -1,6 +1,6 @@
 // Basic bookshop functionality in JavaScript
 ////////////////////////////////////////
-// Sample book dataa//////////////////
+// Sample book dataa/////////////////////
 const books = [
     { id: 1, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', price: 10.99 },
     { id: 2, title: '1984', author: 'George Orwell', price: 8.99 },
